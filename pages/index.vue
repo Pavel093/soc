0cda9e66356fe271b07934fdf717eb899dd91af2
Pavel093 <pavelpichugin555@gmail.com>
2025-10-07 01:16:09 +0500

    ff

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,17 @@
 <script setup>
 import Header from '~/components/Header.vue';
 import HeroSection from '~/components/landing/home/HeroSection.vue';
-import AllCards from '~/components/landing/home/AllCards.vue';
-import AdvantagesSection from '~/components/landing/home/AdvantagesSection.vue';
-import Footer from '~/components/Footer.vue';
-
-
 </script>
 
 <template>
-  <Header />
-  <HeroSection />
-  <AllCards />
-  <AdvantagesSection />
-  <Footer />
+  <div> 
+    <Header />
+    <HeroSection />
+    <LazyAllCards />
+    <LazyAdvantagesSection />
+    <LazyFooter />
+  </div>
 </template>
 
 <style lang="scss">
-
 </style>
