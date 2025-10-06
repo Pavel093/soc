59d62d3979807da8cf478958bc49359d952aabbb
Pavel093 <pavelpichugin555@gmail.com>
2025-10-06 20:40:32 +0500

    ff

--- components/landing/home/HeroSection.vue
+++ components/landing/home/HeroSection.vue
@@ -3,7 +3,7 @@
     <div class="container section-grid">
       <div class="content">
         <h2 class="title">
-          Рассчитайте всё, что положенооооо<br />
+          Рассчитайте всё, что положенаааа<br />
           <span class="title-accent">вашей семье</span>
         </h2>
         
