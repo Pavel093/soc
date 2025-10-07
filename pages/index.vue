6af2564cbf542eb1e221b8b9afa35e4295afabca
Pavel093 <pavelpichugin555@gmail.com>
2025-10-07 20:28:34 +0500

    rr

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,16 @@ import Header from '~/components/Header.vue';
 import HeroSection from '~/components/landing/home/HeroSection.vue';
 import AllCards from '~/components/landing/home/AllCards.vue'
 import AdvantagesSection from '~/components/landing/home/AdvantagesSection.vue'
+
+useHead({
+  title: 'Все калькуляторы пособий и выплат',
+  meta: [
+    {
+      name: 'description',
+      content: 'Бесплатные калькуляторы для расчета всех видов пособий, выплат и льгот. Единое пособие, декретные, алименты, материнский капитал и другие.'
+    }
+  ]
+})
 </script>
 
 <template>
