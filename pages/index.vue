7544d947565c9e3b85df5b4eeb80a3b5069cf934
Pavel093 <pavelpichugin555@gmail.com>
2025-10-07 16:28:51 +0500

    rr

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <script setup>
 import Header from '~/components/Header.vue';
 import HeroSection from '~/components/landing/home/HeroSection.vue';
+import AllCards from '~/components/landing/home/AllCards.vue'
+import AdvantagesSection from '~/components/landing/home/AdvantagesSection.vue'
 </script>
 
 <template>
